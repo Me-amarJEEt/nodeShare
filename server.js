fa12dfe40ed549c95528ba12105500c6a4941310
Amarjeet Kumar <78524159+Me-amarJEEt@users.noreply.github.com>
2023-10-24 20:25:21 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const cors = require('cors');
+
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use(cors(corsOptions))
+const connectDB = require('./config/db');
+
+app.use(express.json());
+
+app.use(express.static('public'));
+// app.use(express.json());
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+// Routes 
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
+
+
+app.listen(PORT, console.log(`Server is listening on port ${PORT}.`));
